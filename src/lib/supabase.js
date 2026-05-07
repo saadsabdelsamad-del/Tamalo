fa8973d9a2fe63d3979d931b320efbb27d2e5f85
Saad <saad.s.abdelsamad@gmail.com>
2026-05-07 14:40:13 +0300

    Switch to legacy anon key for Supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://nzvfgvodibvxgljkiqyk.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_RJtt0Zr5et0Y9vkXBZZ2Ug_BCAotafj'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56dmZndm9kaWJ2eGdsamtpcXlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1MzMzMzksImV4cCI6MjA5MjEwOTMzOX0.YnDrEbgFyd7LyPSIfosReaVFaQZL0po_NksoymLOa_8'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
